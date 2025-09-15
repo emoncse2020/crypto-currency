16614082b5cd01993a5a10f4382d6af75ae44f86
emoncse2020 <emon.cse.2020@gmail.com>
2025-09-15 14:19:43 +0600

    sorting added as well as ui is more dynamic now

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import CoinCard from "./components/CoinCard";
 import LimitSelector from "./components/LimitSelector";
+import FilterInput from "./components/FilterInput";
+import SortSelector from "./components/SortSelector";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -10,6 +12,8 @@ const App = () => {
 
   const [error, setError] = useState(null);
   const [limit, setLimit] = useState(10);
+  const [filter, setFilter] = useState("");
+  const [sortBy, setSortBy] = useState("market_cap_desc");
 
   useEffect(() => {
     const fetchCoins = async () => {
@@ -46,17 +50,50 @@ const App = () => {
     //   });
   }, [limit]);
 
+  const filteredCoins = coins
+    .filter((coin) => {
+      return (
+        coin.name.toLowerCase().includes(filter.toLocaleLowerCase()) ||
+        coin.symbol.toLowerCase().includes(filter.toLocaleLowerCase())
+      );
+    })
+    .slice()
+    .sort((a, b) => {
+      switch (sortBy) {
+        case "market_cap_desc":
+          return b.market_cap - a.market_cap;
+        case "market_cap_asc":
+          return a.market_cap - b.market_cap;
+        case "price_desc":
+          return b.current_price - a.current_price;
+        case "price_asc":
+          return a.current_price - b.current_price;
+        case "change_desc":
+          return b.price_change_percentage_24h - a.price_change_percentage_24h;
+        case "change_asc":
+          return a.price_change_percentage_24h - b.price_change_percentage_24h;
+      }
+    });
+
   return (
     <>
       <h1>🚀 Crypto Currency</h1>
       {loading && <p>Loading ....</p>}
       {error && <div className="error">{error}</div>}
-      <LimitSelector limit={limit} onLimitChange={setLimit}></LimitSelector>
+      <div className="top-controls">
+        <FilterInput filter={filter} onFilterChange={setFilter}></FilterInput>
+        <LimitSelector limit={limit} onLimitChange={setLimit}></LimitSelector>
+        <SortSelector sortBy={sortBy} onSortChange={setSortBy}></SortSelector>
+      </div>
       {!loading && !error && (
         <main className="grid">
-          {coins.map((coin) => (
-            <CoinCard key={coin.id} coin={coin}></CoinCard>
-          ))}
+          {filteredCoins.length > 0 ? (
+            filteredCoins.map((coin) => (
+              <CoinCard key={coin.id} coin={coin}></CoinCard>
+            ))
+          ) : (
+            <p>No Matching Coins</p>
+          )}
         </main>
       )}
     </>
--- src/components/LimitSelector.jsx
+++ src/components/LimitSelector.jsx
@@ -8,10 +8,10 @@ const LimitSelector = ({ limit, onLimitChange }) => {
         id="limit"
         onChange={(e) => onLimitChange(Number(e.target.value))}
       >
-        <option value={5}>5</option>
-        <option value={10}>10</option>
-        <option value={15}>15</option>
-        <option value={20}>20</option>
+        <option value={6}>6</option>
+        <option value={12}>12</option>
+        <option value={18}>18</option>
+        <option value={24}>24</option>
         <option value={100}>100</option>
       </select>
     </div>
